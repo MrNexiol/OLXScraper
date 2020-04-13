8fa736cfeebb97c4865a3a9bdd9979bf396b2db8
Tomasz Kopycinski <mrnexiol@gmail.com>
2020-04-13 23:05:48 +0200

    Added searching through all search pages

--- src/testing/Main.java
+++ src/testing/Main.java
@@ -14,6 +14,9 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.net.URLEncoder;
@@ -30,7 +33,7 @@ public class Main extends Application {
     Button searchButton = new Button("Search");
 
     private class CustomVBox extends VBox {
-        Label titleLabel = new Label();
+        Text titleLabel = new Text();
         Label priceLabel = new Label();
 
         public CustomVBox(String title, String price){
@@ -38,6 +41,8 @@ public class Main extends Application {
 
             this.titleLabel.setText(title);
             this.priceLabel.setText(price);
+
+            this.titleLabel.setFont(Font.font("Verdana", FontWeight.BOLD,12));
 
             this.getChildren().addAll(titleLabel,priceLabel);
         }
@@ -66,12 +71,21 @@ public class Main extends Application {
             try {
                 String searchUrl = "https://www.olx.pl/gorzow/q-" +
                         URLEncoder.encode(searchQuery, StandardCharsets.UTF_8) +
-                        "/?search%5Border%5D=filter_float_price%3Aasc";
-                HtmlPage page = client.getPage(searchUrl);
-                articles = page.getByXPath("//table[@id='offers_table']/tbody/tr[@class='wrap']");
-                if (articles.isEmpty()) {
-                    System.out.println("No articles");
-                } else {
+                        "/?search%5Border%5D=filter_float_price%3Aasc&search%5Bdist%5D=50";
+                HtmlPage website = client.getPage(searchUrl);
+                String pageCount = website.getFirstByXPath("string(//div[@class='pager rel clr']/span[@class='item fleft'][last()]/a/span)");
+
+                int integerPage;
+                try {
+                    integerPage = Integer.parseInt(pageCount);
+                }catch (Exception e){
+                    integerPage = 1;
+                }
+
+                for (int i = 0; i < integerPage; i++) {
+                    searchUrl += "&page=" + ++i;
+                    HtmlPage page = client.getPage(searchUrl);
+                    articles = page.getByXPath("//table[@id='offers_table']/tbody/tr[@class='wrap']");
                     for (HtmlElement element : articles){
                         String title = element.getFirstByXPath("string(descendant::img/@alt)");
                         String price = element.getFirstByXPath("string(descendant::p[@class='price']/strong)");
@@ -95,7 +109,7 @@ public class Main extends Application {
         mainPane.setLeft(leftPane);
         mainPane.setCenter(listView);
 
-        Scene scene = new Scene(mainPane,600,600);
+        Scene scene = new Scene(mainPane,700,600);
 
         stage.setScene(scene);
 
